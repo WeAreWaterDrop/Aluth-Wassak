f122f1198ed69063fbf9fc15287a5164d3e983a4
Okith Moksha <96955211+OkithM@users.noreply.github.com>
2024-07-29 06:42:16 +0530

    New audio player and Img preview

--- script.js
+++ script.js
@@ -1,179 +1,323 @@
+// Music player
 const songs = [
-    './Song/Ammage Adare.mp3',
-    './Song/Anantha U ahasa mamai.mp3',
-    './Song/Divi gamane.mp3',
-    './Song/Edakale Nurthi.mp3',
-    './Song/Kola Kamata.mp3',
-    './Song/Met Bosat.mp3',
-    './Song/Piya pataka liyala.mp3',
-    './Song/Rahase horahin.mp3',
-    './Song/Senehas Aranata.mp3',
-    './Song/Senehasa ella.mp3',
-    './Song/Shudhatmayanani.mp3',
-    './Song/Sihina Atarin.mp3'
-    // Add paths to your songs here
+  "./Song/Ammage Adare.mp3",
+  "./Song/Anantha U ahasa mamai.mp3",
+  "./Song/Divi gamane.mp3",
+  "./Song/Edakale Nurthi.mp3",
+  "./Song/Kola Kamata.mp3",
+  "./Song/Met Bosat.mp3",
+  "./Song/Piya pataka liyala.mp3",
+  "./Song/Rahase horahin.mp3",
+  "./Song/Senehas Aranata.mp3",
+  "./Song/Senehasa ella.mp3",
+  "./Song/Shudhatmayanani.mp3",
+  "./Song/Sihina Atarin.mp3",
+  // Add paths to your songs here
 ];
 const SongNames = [
-    'Ammage Adare',
-    'Anantha U ahasa mamai',
-    'Divi gamane',
-    'Edakale Nurthi',
-    'Kola Kamata',
-    'Met Bosat',
-    'Piya pataka liyala',
-    'Rahase horahin',
-    'Senehas Aranata',
-    'Senehasa ella',
-    'Shudhatmayanani',
-    'Sihina Atarin'
-    // Add names of your songs here
+  "Ammage Adare",
+  "Anantha U ahasa mamai",
+  "Divi gamane",
+  "Edakale Nurthi",
+  "Kola Kamata",
+  "Met Bosat",
+  "Piya pataka liyala",
+  "Rahase horahin",
+  "Senehas Aranata",
+  "Senehasa ella",
+  "Shudhatmayanani",
+  "Sihina Atarin",
+  // Add names of your songs here
 ];
 
-const playPause = [
-    './PlayerControls/play.png',
-    './PlayerControls/pause.png'
-]
+const playPause = ["./PlayerControls/play.svg", "./PlayerControls/pause.svg"];
 
 let currentSongIndex = 0;
-const audioPlayer = document.getElementById('audioPlayer');
-const nextButton = document.getElementById('nextButton');
-const prevButton = document.getElementById('prevButton');
-const playButton = document.getElementById('playButton');
-const progressBar = document.getElementById('song-progress');
-const playButtonImg = document.getElementById('playButtonImg');
-const volumeSlider = document.getElementById('volumeSlider');
+const mainPlayer = document.getElementById("mainPlayer");
+const songTitle = document.getElementById("musicTitle");
+const musicPlayer = document.getElementById("musicPlayer");
+const musicControls = document.getElementById("musicControls");
+const musicProgress = document.getElementById("musicProgress");
+const musicPlay = document.getElementById("musicPlay");
+const musicNext = document.getElementById("musicNext");
+const musicPrev = document.getElementById("musicPrev");
+const musicPlayImg = document.getElementById("musicPlayImg");
+const musicVolumeRange = document.getElementById("musicVolumeRange");
 
-function changeTitle() {
-    document.getElementById('song-title').innerHTML = "<b>" + SongNames[currentSongIndex] + "</b>";
+musicPlay.src = playPause[0];
+function changeSong() {
+  mainPlayer.src = songs[currentSongIndex];
+  songTitle.innerHTML = SongNames[currentSongIndex];
+}
+function nextSong() {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  changeSong();
+  mainPlayer.play();
+  musicPlayImg.src = playPause[1];
+}
+function prevSong() {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  changeSong();
+  mainPlayer.play();
+  musicPlayImg.src = playPause[1];
+}
+function playPauseSong() {
+  if (mainPlayer.paused) {
+    mainPlayer.play();
+    musicPlayImg.src = playPause[1];
+  } else {
+    mainPlayer.pause();
+    musicPlayImg.src = playPause[0];
+  }
 }
 
-// Function to update the progress bar range
-function updateProgressBar() {
-    progressBar.max = audioPlayer.duration;
-    progressBar.value = audioPlayer.currentTime;
-}
+addEventListener("load", changeSong);
+mainPlayer.pause();
+musicNext.addEventListener("click", nextSong);
+musicPrev.addEventListener("click", prevSong);
+musicPlay.addEventListener("click", playPauseSong);
 
-// Function to play the current song
-function playSong(index) {
-    audioPlayer.src = songs[index];
-    audioPlayer.play();
-}
+// volume
 
-// Change play/pause button image
-function changePlayPause() {
-    if (audioPlayer.paused) {
-        playButtonImg.src = playPause[0];
-    } else {
-        playButtonImg.src = playPause[1];
-    }
-}
-
-// Change song volume
+musicVolumeRange.value = mainPlayer.volume * 100;
 function changeVolume() {
-    audioPlayer.volume = volumeSlider.value / 100;
+  mainPlayer.volume = musicVolumeRange.value / 100;
 }
 
-// Event listener for the volume slider
-volumeSlider.addEventListener('input', changeVolume);
+musicVolumeRange.addEventListener("change", changeVolume);
 
-// Event listener for the Next button
-nextButton.addEventListener('click', () => {
-    currentSongIndex = (currentSongIndex + 1) % songs.length;
-    playSong(currentSongIndex);
-    changeTitle();
+// Progress
+
+function updateProgress() {
+  musicProgress.value = (mainPlayer.currentTime / mainPlayer.duration) * 100;
+}
+
+mainPlayer.addEventListener("timeupdate", updateProgress);
+
+musicProgress.addEventListener("change", () => {
+  mainPlayer.currentTime = (musicProgress.value * mainPlayer.duration) / 100;
+  if (mainPlayer.ended) {
+    nextSong();
+  }
+  mainPlayer.play();
+  musicPlayImg.src = playPause[1];
 });
 
-// Event listener for the Previous button
-prevButton.addEventListener('click', () => {
-    currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-    playSong(currentSongIndex);
-    changeTitle();
-});
-
-// Event listener for the Play/Pause button
-playButton.addEventListener('click', () => {
-    if (audioPlayer.paused) {
-        audioPlayer.play();
-    } else {
-        audioPlayer.pause();
-    }
-    changePlayPause();
-});
-
-// Event listener to update progress bar when song is playing
-audioPlayer.addEventListener('timeupdate', () => {
-    progressBar.value = audioPlayer.currentTime;
-});
-
-// Event listener to update progress bar range and duration display when metadata is loaded
-audioPlayer.addEventListener('loadedmetadata', () => {
-    updateProgressBar();
-});
-
-// Event listener to seek the song when progress bar is changed
-progressBar.addEventListener('change', () => {
-    audioPlayer.currentTime = progressBar.value;
-    audioPlayer.play();
-    changePlayPause();
-});
-
-// Run function to change the song and play it
-playSong(currentSongIndex);
-changeTitle();
-
-// Change volume slider value
-volumeSlider.value = audioPlayer.volume * 100;
-
-// Stop autoplay initially
-audioPlayer.pause();
-
-changePlayPause();
-
-
+setInterval(function () {
+  updateProgress();
+}, 1000);
 
 /////////////////////////////////////////////////////////////////////////
 
 // Video Player
 
-
 const videos = [
-    './video/1.mp4',
-    './video/2.mp4',
-    './video/3.mp4',
-    './video/4.mp4',
-    './video/5.mp4'
+  "./video/1.mp4",
+  "./video/2.mp4",
+  "./video/3.mp4",
+  "./video/4.mp4",
+  "./video/5.mp4"
 ];
 
 let currentVideoIndex = 0;
-const videoPlayer = document.getElementById('headerVideoPlayer');
-const source = videoPlayer.getElementsByTagName('source')[0];
-const vprevButton = document.getElementById('videoPrev');
-const vnextButton = document.getElementById('videoNext');
+const videoPlayer = document.getElementById("headerVideoPlayer");
+const source = videoPlayer.getElementsByTagName("source")[0];
+const vprevButton = document.getElementById("videoPrev");
+const vnextButton = document.getElementById("videoNext");
 
 function changeVideo() {
-    source.src = videos[currentVideoIndex];
-    videoPlayer.load();
+  source.src = videos[currentVideoIndex];
+  videoPlayer.load();
 }
 
 function nextVideo() {
-    currentVideoIndex = (currentVideoIndex + 1) % videos.length;
-    changeVideo();
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+  changeVideo();
 }
 
 function prevVideo() {
-    currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
-    changeVideo();
+  currentVideoIndex = (currentVideoIndex - 1 + videos.length) % videos.length;
+  changeVideo();
 }
 
-vprevButton.addEventListener('click', prevVideo);
-vnextButton.addEventListener('click', nextVideo);
+vprevButton.addEventListener("click", prevVideo);
+vnextButton.addEventListener("click", nextVideo);
 
 function checkVideoEnd() {
-    if (videoPlayer.currentTime >= videoPlayer.duration-1) {
-        nextVideo();
-    }
+  if (videoPlayer.currentTime >= videoPlayer.duration - 1) {
+    nextVideo();
+  }
 }
 
 setInterval(checkVideoEnd, 500); // Check every second if the video has ended
 
 changeVideo();
+
+//Prewiew image
+
+const img = document.getElementById("imagePreviewImg");
+const closeBtn = document.getElementById("imagePreviewClose");
+const prewWIndow = document.getElementById("imagePreview");
+let scale = 1;
+let isDragging = false;
+let startX, startY, originX, originY;
+let initialDistance = 0;
+let initialScale = 1;
+
+img.addEventListener("wheel", (e) => {
+  e.preventDefault();
+  scale += e.deltaY * -0.01;
+  scale = Math.min(Math.max(0.5, scale), 3);
+  img.style.transform = `scale(${scale})`;
+});
+
+img.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  startX = e.pageX - img.offsetLeft;
+  startY = e.pageY - img.offsetTop;
+  originX = img.offsetLeft;
+  originY = img.offsetTop;
+  img.style.cursor = "grabbing";
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  img.style.cursor = "grab";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    img.style.left = `${e.pageX - startX}px`;
+    img.style.top = `${e.pageY - startY}px`;
+  }
+});
+
+img.addEventListener("touchstart", (e) => {
+  if (e.touches.length === 1) {
+    isDragging = true;
+    startX = e.touches[0].pageX - img.offsetLeft;
+    startY = e.touches[0].pageY - img.offsetTop;
+    originX = img.offsetLeft;
+    originY = img.offsetTop;
+  } else if (e.touches.length === 2) {
+    initialDistance = getDistance(e.touches[0], e.touches[1]);
+    initialScale = scale;
+  }
+});
+
+img.addEventListener("touchmove", (e) => {
+  if (e.touches.length === 1 && isDragging) {
+    e.preventDefault();
+    img.style.left = `${e.touches[0].pageX - startX}px`;
+    img.style.top = `${e.touches[0].pageY - startY}px`;
+  } else if (e.touches.length === 2) {
+    e.preventDefault();
+    const currentDistance = getDistance(e.touches[0], e.touches[1]);
+    scale = initialScale * (currentDistance / initialDistance);
+    scale = Math.min(Math.max(0.5, scale), 3);
+    img.style.transform = `scale(${scale})`;
+  }
+});
+
+document.addEventListener("touchend", () => {
+  isDragging = false;
+});
+
+function getDistance(touch1, touch2) {
+  const dx = touch1.pageX - touch2.pageX;
+  const dy = touch1.pageY - touch2.pageY;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+closeBtn.addEventListener("click", () => {
+  prewWIndow.style.display = "none";
+  img.style.left = 0;
+  img.style.top = 0;
+  img.style.transform = `scale(1)`;
+  img.style.width = "100%";
+});
+
+// Images
+
+const imgList = [
+  "./image/1.jpg",
+  "./image/2.jpg",
+  "./image/3.jpg"
+];
+
+let currentImgIndex = 0;
+
+const prgmImgs = document.getElementById("prgmImgs");
+
+prgmImgs.src = imgList[currentImgIndex];
+
+prgmImgs.addEventListener("click", () => {
+  prewWIndow.style.display = "block";
+  img.src = prgmImgs.src;
+});
+
+const imagesDots = document.getElementById("imagesDots");
+let buttons = "";
+
+for (let i = 0; i < imgList.length; i++) {
+  buttons += "<button class='imageDot'" +" id=" + imgList[i]+ " name=" + i + "></button>";
+}
+
+imagesDots.innerHTML = buttons;
+
+// Get all buttons with the class 'output-button'
+const imageDot = document.querySelectorAll(".imageDot");
+
+function changeDotSize(x){
+    document.getElementById(x).style.scale = 1.5;
+}
+
+function retetButtons(){
+    imageDot.forEach((imageDot) => {
+        imageDot.style.scale = 1;
+    });
+}
+
+function nextImg(){
+    currentImgIndex = (currentImgIndex + 1) % imgList.length;
+    prgmImgs.src = imgList[currentImgIndex];
+    retetButtons();
+    changeDotSize(imgList[currentImgIndex]);
+}
+
+function prevImg(){
+    currentImgIndex = (currentImgIndex - 1 + imgList.length) % imgList.length;
+    prgmImgs.src = imgList[currentImgIndex];
+    retetButtons();
+    changeDotSize(imgList[currentImgIndex]);
+}
+
+// Loop through each button and add a click event listener
+imageDot.forEach((imageDot) => {
+  imageDot.addEventListener("click", () => {
+    retetButtons();
+    prgmImgs.src = imageDot.id;
+    changeDotSize(imageDot.id);
+    currentImgIndex = imageDot.name;
+  });
+});
+
+changeDotSize("./image/1.jpg");
+
+setInterval(nextImg, 10000);
+
+// Swipe functionality
+let stX;
+
+prgmImgs.addEventListener("touchstart", (e) => {
+  stX = e.touches[0].clientX;
+});
+
+prgmImgs.addEventListener("touchend", (e) => {
+  const enX = e.changedTouches[0].clientX;
+  if (stX > enX + 50) {
+    nextImg(); // Swipe left
+  } else if (stX < enX - 50) {
+    prevImg(); // Swipe right
+  }
+});
